71777ef7fe11daae38ecb41f9517b6635f4fdc13
jlneuser <jlneuser@outlook.com>
2024-11-23 23:21:03 -0600

    add reset button and beginning of "current color" text

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 // identifying the button
 
-const button = document.getElementById('colorBlue');
+const button = document.getElementById('colorButton');
 
 button.addEventListener('click', () => {
+  // creating my color palette to choose from
   const colors = ['blue', 'red', 'purple', 'green', 'pink'];
+  // using colors[] to choose from the color palette 
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  // finally, painting onto the background the color chosen
   document.body.style.backgroundColor = randomColor;
 });
+
+// **!I DID THIS ONE BY MYSELF!**
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', () => {
+  const backToWhite = document.body.style.backgroundColor = 'white';
+})
+
